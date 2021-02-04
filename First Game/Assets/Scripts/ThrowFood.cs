7e112d189b168721f8ebbe21ebf09c32c8eea90f
Mitchell <mlrotenberry99@yahoo.com>
2021-02-03 20:14:47 -0600

    added prefab and made food be thrown

--- First Game/Assets/Scripts/ThrowFood.cs
+++ First Game/Assets/Scripts/ThrowFood.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class ThrowFood : MonoBehaviour
 {
+
+    public GameObject food;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("throwing");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator throwing()
+    {
+        yield return new WaitForSeconds(2f);
+        for (; ; )
+        {
+            Instantiate(food);
+            yield return new WaitForSeconds(2.5f);
+        }
     }
 }
