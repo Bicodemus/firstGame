49c93dde5ebc69a764480c90906d3af74cdd7c56
Mitchell <mlrotenberry99@yahoo.com>
2021-02-03 19:50:32 -0600

    Fixed more files on my end

---
+++ First Game/Assets/Scripts/FoodMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodMover : MonoBehaviour
+{
+
+    private Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        body.AddForce(transform.right * 135);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
