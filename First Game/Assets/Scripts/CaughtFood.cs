669ac2c874aaec4c9c1c2557b34c0f20e96601b5
Mitchell <mlrotenberry99@yahoo.com>
2021-02-03 22:45:24 -0600

    made alot of changes, tried to get plate to catch food.
    somewhat working

---
+++ First Game/Assets/Scripts/CaughtFood.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaughtFood : MonoBehaviour
+{
+    private Rigidbody2D plate;
+    private GameObject food;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        plate = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        food = collision.gameObject;
+        food.GetComponent<Rigidbody2D>().isKinematic = true;
+        food.transform.SetParent(plate.transform);
+        food.tag = "onPlate";
+        Debug.Log("hit");
+        Debug.Log("hit2");
+    }
+}
---
+++ First Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance { get; private set; }
+    private int score = 0;
+    public TextMeshProUGUI scoreText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Working";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
