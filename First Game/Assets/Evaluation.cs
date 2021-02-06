f45c5b89c30deb0e1a12ce0b6b7ecf23f9e51013
Mitchell <mlrotenberry99@yahoo.com>
2021-02-05 19:58:24 -0600

    updated unity

---
+++ First Game/Assets/Evaluation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evaluation : MonoBehaviour
+{
+
+    /* This project was created as our first introduction to unity. We were tasked with using at least one of the 3 words, Bear, Pancake, and Bird. For our game we chose the word pancake.
+     * As this project was designed to be an introduction, it is fairly simple. The goal of the game is to catch pancakes that are being tossed onto a plate. You will just want to see how many
+     * pancakse you can catch. When you catch one, your score is incremented. If one drops to the floor, the score is decremented. The way the game works is by moving your mouse left and right
+     * to catch the pancakes. The pancakes stick to the plate, and stack on top of each other.
+     * 
+     * We were not able to finish the game due to technial errors, but we intended to make the scene look like a dinner, and have chefs throwing the pancakes at us. 
+     * 
+     * This project was fun. I had no experience with unity prior to this, and i learned a lot. I learned how to make different objects interact, although gettting the interacitons that i 
+     * desired was a struggle. 
+     */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
